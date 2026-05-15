79718a1c5c446f078c9c4e6aeb6ffe943e2ffa3c
Jakub <jakubokrasa3@gmail.com>
2026-05-16 01:58:16 +0200

    Add Meta Pixel to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -171,6 +171,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://images.unsplash.com" />
+        {/* Meta Pixel */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init','27519887457647449');
+          fbq('track','PageView');
+        `}} />
+        <noscript><img height="1" width="1" style={{display:'none'}}
+          src="https://www.facebook.com/tr?id=27519887457647449&ev=PageView&noscript=1" /></noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
